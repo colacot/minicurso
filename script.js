8f3094c4be87a1d03912094f7dce0059ccdb97ad
Colaco <colacotc@gmail.com>
2022-01-16 17:05:06 -0300

    Introduzir tradução do item list Status

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
 
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return ' Desconhecido';
+    }else if(data.status == 'Alive'){
+        return ' Sim';
+    }else {
+        return ' Não. Está morto';
+    }
+}
+
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 }
@@ -21,7 +31,7 @@ pegarPersonagem = () => {
         imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name;
         especie.innerHTML = data.species;
-        condicao.innerHTML = data.status;
+        condicao.innerHTML = traduzirCondicao(data);
     });
 }
 
